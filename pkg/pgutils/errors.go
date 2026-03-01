a85747d3bddbd6ea2a7e9e5a146d02d0e2536e69
Purinutt Amartayavis <ice.ice5335@gmail.com>
2026-03-01 17:15:22 +0700

    Add: pgutils#CheckError

--- pkg/pgutils/errors.go
+++ pkg/pgutils/errors.go
@@ -1,7 +1,60 @@
 package pgutils
 
-import "errors"
+import (
+	"errors"
+	"log"
+	"net/http"
+
+	"github.com/jackc/pgx/v5"
+	"github.com/jackc/pgx/v5/pgconn"
+)
 
 var (
-	ErrInvalidType = errors.New("Invalid Input type")
+	ErrInvalidType         = errors.New("Invalid Input type")
+	ErrNoRowsAffected      = errors.New("No rows affected")
+	ErrForeignKeyViolation = errors.New("Foreign Key Violation")
+	ErrUniqueViolation     = errors.New("Unique Key Violation")
+	ErrNotNullViolation    = errors.New("Not Null Violation")
 )
+
+func CheckError(err error, w http.ResponseWriter) error {
+	if err == nil {
+		return nil
+	}
+
+	var pgErr *pgconn.PgError
+	var status int
+
+	switch {
+	case errors.Is(err, ErrNoRowsAffected):
+		status = http.StatusNotFound
+	case errors.Is(err, pgx.ErrNoRows):
+		status = http.StatusNotFound
+	case errors.Is(err, pgx.ErrTooManyRows):
+		status = http.StatusInternalServerError
+	case errors.As(err, &pgErr):
+		switch pgErr.Code {
+		case "23502":
+			status = http.StatusConflict
+			err = ErrNotNullViolation
+		case "23503":
+			status = http.StatusConflict
+			err = ErrForeignKeyViolation
+		case "23505":
+			status = http.StatusConflict
+			err = ErrUniqueViolation
+		default:
+			log.Println(err)
+			status = http.StatusInternalServerError
+		}
+	default:
+		log.Println(err)
+		status = http.StatusInternalServerError
+	}
+
+	if w != nil {
+		w.WriteHeader(status)
+	}
+
+	return err
+}
